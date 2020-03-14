28199f09d66db669a4671da897b746dbab9e3eb6
venkat1702 <52599813+venkat1702@users.noreply.github.com>
2020-03-14 07:53:57 +0530

    Add files via upload

---
+++ Sentiment_Analysis.py
@@ -0,0 +1,26 @@
+import tweepy
+import pandas as pd
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+#Twitter API Authentication Variables
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+tweets = api.search('Artificial Intelligence', count=200)
+data = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
+print(data.head(10))
+print(tweets[0].created_at)
+nltk.download('vader_lexicon')
+sid = SentimentIntensityAnalyzer()
+listy = []
+for index, row in data.iterrows():
+  ss = sid.polarity_scores(row["Tweets"])
+  listy.append(ss)
+#storing the scores into a csv file using pandas
+se = pd.Series(listy)
+data['polarity'] = se.values
+print(data.head(100))
